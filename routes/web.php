6cdb96e94c11574c4a0a03aa2e0bfb811b5c7042
YosephHailu <hailuyoseph9@gmail.com>
2022-07-24 11:14:25 +0300

    feat: add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,25 +13,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('/users', App\Http\Controllers\UserController::class)->names('user');
+    Route::get('/admins', [App\Http\Controllers\UserController::class, 'admins'])->name('user.admins');
+    Route::get('/store/mystore', [App\Http\Controllers\StoreController::class, 'myStore'])->name('store.mystore');
+    Route::get('/store/{store}/login', [App\Http\Controllers\StoreController::class, 'login'])->name('store.login');
+    Route::get('/store/{user}/logout', [App\Http\Controllers\StoreController::class, 'logout'])->name('store.logout');
+    Route::resource('/product_categories', App\Http\Controllers\ProductCategoryController::class)->names('product_category');
+    Route::get('product_categories/{productCategory}/toggle', [App\Http\Controllers\ProductCategoryController::class, 'toggle'])->name('product_category.toggle');
+});
+
 Route::get('/', [App\Http\Controllers\HomeController::class, 'landing'])->name('landing');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/coming-soon', [App\Http\Controllers\HomeController::class, 'comingSoon'])->name('coming-soon');
-Route::resource('/users', App\Http\Controllers\UserController::class)->names('user');
-Route::get('/admins', [App\Http\Controllers\UserController::class, 'admins'])->name('user.admins');
-Route::get('/store/mystore', [App\Http\Controllers\StoreController::class, 'myStore'])->name('store.mystore');
-Route::get('/store/{store}/login', [App\Http\Controllers\StoreController::class, 'login'])->name('store.login');
-Route::get('/store/{user}/logout', [App\Http\Controllers\StoreController::class, 'logout'])->name('store.logout');
 Route::resource('/stores', App\Http\Controllers\StoreController::class)->names('store');
-Route::resource('/product_categories', App\Http\Controllers\ProductCategoryController::class)->names('product_category');
-Route::get('product_categories/{productCategory}/toggle', [App\Http\Controllers\ProductCategoryController::class, 'toggle'])->name('product_category.toggle');
 Route::resource('/products', App\Http\Controllers\ProductController::class)->names('product');
 Route::resource('/orders', App\Http\Controllers\OrderController::class)->names('order');
 
